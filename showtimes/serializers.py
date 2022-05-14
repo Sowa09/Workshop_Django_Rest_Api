297eb45ade853694832c4b31e6543a5ab297c412
Sowa09 <tsowa09@gmail.com>
2022-05-14 16:51:23 +0200

    screening serializers

--- showtimes/serializers.py
+++ showtimes/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
-from showtime.models import Cinema, Screening
+from .models import Cinema, Screening
+from movielist.models import Movie
 
 
 class CinemaSerializer(serializers.ModelSerializer):
-    movies = serializers.HyperlinkedRelatedField(many=True, slug_field='movies_details')
+    movies = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='movies-details')
 
     class Meta:
         model = Cinema
-        fields = ['__all__']
+        fields = ['name', 'city', 'movies']
 
 
+class ScreeningSerializer(serializers.ModelSerializer):
+    cinema = serializers.SlugRelatedField(slug_field='name', queryset=Cinema.objects.all())
+    movie = serializers.SlugRelatedField(slug_field='title', queryset=Movie.objects.all())
+
+    class Meta:
+        model = Screening
+        fields = ['movie', 'cinema', 'date']
